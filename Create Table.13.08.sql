3f1cab86dad769e74d2cdd253f7f5f49d042d5b8
Theuszadaa <159173070+Theuszadaa@users.noreply.github.com>
2024-08-13 13:46:09 -0300

    Código SQL do dia 13/08

---
+++ Create Table.13.08.sql
@@ -0,0 +1,6 @@
+CREATE TABLE alunos(
+	nome varchar(25) NOT NULL,
+  	turma varchar(10),
+  	id int(3) UNIQUE,
+    idade varchar(2)
+);
---
+++ dump.sqlite (1).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+	nome varchar(25) NOT NULL,
+  	turma varchar(10),
+  	id int(3) UNIQUE,
+    idade varchar(2)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
